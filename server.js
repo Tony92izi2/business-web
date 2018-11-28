260b86e8fcd47c5fecfc6ed34d05ed28bdaef8cb
Antoine Keita <keita_antoine@yahoo.fr>
2018-11-29 00:56:10 +0100

    first commit 3

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-deploy'));
+app.use(express.static(__dirname + '/dist/business-web'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angular-deploy/index.html'));
+res.sendFile(path.join(__dirname+'/dist/business-web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 // app.listen(process.env.PORT || 8080);
 // use port 3000 unless there exists a preconfigured port
-
+console.log("running on : http://localhost:8080")
 app.listen(process.env.port || 8080);
