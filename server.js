6a3c0fc11a79147c8e0a5ef3a4f5dd8c5502b6d7
Antoine Keita <keita_antoine@yahoo.fr>
2018-11-29 20:32:43 +0100

    commit 7

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ res.sendFile(pngath.join(__dirname+'/dist/index.html'));
 // Start the app by listening on the default Heroku port
 // app.listen(process.env.PORT || 8080); ok
 // use port 3000 unless there exists a preconfigured port
-// console.log("running on : http://localhost:8080")
-// app.listen(process.env.port || 8080);
+console.log("running on : http://localhost:8080")
+app.listen(process.env.port || 8080);
